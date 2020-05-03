a4d1e32e294208e84b3821d5330820521e681471
yunu2403 <63942216+yunu2403@users.noreply.github.com>
2020-05-03 13:21:52 +0900

    20200503

---
+++ myHairJoint/Assets/Script/ButtonMenu.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonMenu : MonoBehaviour
+{
+    public GameObject hairPrefab;
+    public int[] hairList;  
+
+    public void IsKinematicChange()
+    {
+        Rigidbody _rigidbody = GetComponentInChildren<Rigidbody>();
+        var parent = new GameObject("hairs");
+        var hair = Instantiate(hairPrefab);
+
+        var script = hair.GetComponent<Hair>();
+        script.SetRigidBody(_rigidbody);
+
+        foreach (var item in hairList)
+        {
+            if (hairList.IsReadOnly)
+            {
+                var generation = hairPrefab.GetComponent<HairGeneration>();
+
+
+                //hairList = _rigidbody.r
+                //last= hair.GetComponent<Hair>();
+            }
+
+        }
+
+        _rigidbody.isKinematic = false;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ myHairJoint/Assets/Script/Starter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    public GameObject hairRootPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 currentPos = transform.position;
+        for (int i = 0; i < 40; i++)
+        {
+            var root = Instantiate(hairRootPrefab);
+            root.transform.position = currentPos;
+            currentPos += Vector3.right * .1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

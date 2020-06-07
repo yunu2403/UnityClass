4de87aecff584932f3bd71e92dbc9a2fbfdf5dd4
yunu2403 <63942216+yunu2403@users.noreply.github.com>
2020-06-07 14:08:06 +0900

    ...

---
+++ myHairJoint_Class_200531/Assets/Script/Manager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+
+    public static HairGeneration currentHairGeneration;
+    public Button button;
+
+
+    private LayerMask _layerMaskRoot;
+
+
+    public void OnClickMakeRoot()
+    {
+        if (MouseCapture.Instance.selectArea.activeSelf == false)
+            return;
+
+        var obj = Instantiate(MouseCapture.Instance.hairGenrationPrefab);
+        obj.transform.position = MouseCapture.Instance.selectArea.transform.position;
+        obj.transform.rotation = MouseCapture.Instance.selectArea.transform.rotation;
+        MouseCapture.Instance.selectArea.SetActive(false);
+
+        var script = obj.GetComponentInChildren<HairStartCube>();
+        script.SyncPosition();
+
+        HairGeneration.SelectItem(obj.GetComponent<HairGeneration>());
+    }
+
+    //public void OnClickNextItem()
+    //{
+    //    var index = HairGeneration._List.IndexOf(currentHairGeneration);
+
+    //    index++;
+    //    if (index > HairGeneration._List.Count - 1)
+    //    {
+    //        index = 0;
+    //    }
+
+    //    HairGeneration.SelectItem(HairGeneration._List[index]);
+
+    //}
+
+    public void OnClickMakeHair()
+    {
+        if (currentHairGeneration != null)
+        {
+            currentHairGeneration.MakeHair();
+        }
+    }
+
+    public void OnClickDeleteHair()
+    {
+        if (currentHairGeneration != null)
+        {
+            currentHairGeneration.DeleteHair();
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
